684e272c0c4e15f6a24b30687b8ce1549f3aa32c
Shivang <kshivang679@gmail.com>
2026-03-10 19:35:21 +0530

    fix: update S3 bucket name and EKS cluster name in Terraform configuration

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "terra-eks12"
+    bucket         	   = "cosmic-view-terraform-eks-state-bucket "
     key              	   = "state/terraform.tfstate"
     region         	   = "us-east-1"
   }
