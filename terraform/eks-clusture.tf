755643658adf288e6a0cf7b5329d7335faf86597
Shivang <kshivang679@gmail.com>
2026-03-18 16:50:13 +0530

    fix: restructure EKS module configuration and add authentication exec block

--- terraform/eks-clusture.tf
+++ terraform/eks-clusture.tf
@@ -5,14 +5,41 @@ module "eks" {
   name               = local.cluster_name
   kubernetes_version = "1.33"
 
-    enable_cluster_creator_admin_permissions = true
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  enable_cluster_creator_admin_permissions = true
 
-  endpoint_public_access = true
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
+  endpoint_private_access = true
+  endpoint_public_access  = true
 
 
+  access_entries = {
+    jenkins_role = {
+      kubernetes_groups = []
+      # Use the exact ARN shown in your IAM console screenshot:
+      principal_arn     = "arn:aws:iam::109714237020:role/jenkins-to-eks-role" 
+      
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
